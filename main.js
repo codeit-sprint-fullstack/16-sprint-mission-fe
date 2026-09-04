adec62cdc9b82e2e319a72a289d7fff1a0111b42
iroNomad <jaka0926@naver.com>
2026-09-04 14:26:54 +0900

    Add article API call functions for fetching and creating articles

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const baseURL = "https://panda-market-api-crud.vercel.app";
+const CONTENT_TYPE = "application/json";
+const HTTP_METHODS = {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    DELETE: "DELETE"
+}
+
+// Article API call functions
 
 const getArticleList = async (page, pageSize, keyword) => {
     const params = new URLSearchParams({
@@ -6,15 +15,48 @@ const getArticleList = async (page, pageSize, keyword) => {
         pageSize: pageSize || 10,
         keyword: keyword || "",
     });
-
     const data = await fetch(`${baseURL}/articles?${params}`, {
-        method: "GET",
+        method: HTTP_METHODS.GET,
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": CONTENT_TYPE,
         }
     })
         .then(response => response.json());
 
     console.log(data);
 }
-getArticleList();
+
+
+const getArticle = async (id) => {
+    const data = await fetch(`${baseURL}/articles/${id}`, {
+        method: HTTP_METHODS.GET,
+        headers: {
+            "Content-Type": CONTENT_TYPE,
+        }
+    })
+        .then(response => response.json());
+
+    console.log(data);
+}
+
+const createArticle = async (title, content, image) => {
+    const articleData = {
+        title,
+        content,
+        image
+    };
+    const data = await fetch(`${baseURL}/articles`, {
+        method: HTTP_METHODS.POST,
+        headers: {
+            "Content-Type": CONTENT_TYPE,
+        },
+        body: JSON.stringify(articleData)
+    })
+        .then(response => response.json());
+
+    console.log(data);
+}
+
+// getArticleList();
+// getArticle(4312);
+// createArticle("New Article", "This is the content of the new article.", "https://example.com/image.jpg");
